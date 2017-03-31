8ee38c47d8ee2ff6769c29e765e6e74b76b88ad5
Konstantin Belousov <kib@freebsd.org>
2017-03-31 18:58:34 +0300

    Reduce diffs.

--- lib/libc/gen/scandir-compat11.c
+++ lib/libc/gen/scandir-compat11.c
@@ -10,7 +10,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -87,6 +87,7 @@ freebsd11_scandir(const char *dirname, struct freebsd11_dirent ***namelist,
 	if ((dirp = opendir(dirname)) == NULL)
 		return(-1);
 
+	numitems = 0;
 	arraysz = 32;	/* initial estimate of the array size */
 	names = (struct freebsd11_dirent **)malloc(
 	    arraysz * sizeof(struct freebsd11_dirent *));
